4cf3fa70951c727740a190f09bcfc10a260d368f
Fat-Cow <dennischow07@gmail.com>
2015-01-30 16:34:29 +0800

    Addded a new package

--- simple-todos.js
+++ simple-todos.js
@@ -2,67 +2,124 @@ Tasks = new Mongo.Collection("tasks");
 // console.log( Tasks ); 
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  // Session.setDefault('counter', 0);
+	// counter starts at 0
+	// Session.setDefault('counter', 0);
 
 
-  Template.body.helpers({
-    tasks: function () {
-      // return Tasks.find({});
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
+	Template.body.events({
+		"change .hide-completed input": function (event) {
+			console.log('hello');
+			Session.set("hideCompleted", event.target.checked);
+		}
+	});
+
+	Template.body.helpers({
+		// tasks: function () {
+		//   // return Tasks.find({});
+		//   return Tasks.find({}, {sort: {createdAt: -1}});
+		// },
+		totalCount: function(){
+
+			return Tasks.find({}).count();
+
+		},
+		tasks: function(){
+
+			if (Session.get("hideCompleted")) {
+
+				// If hide completed is checked, filter tasks
+				return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+
+			}else{
+
+				// Otherwise, return all of the tasks
+				return Tasks.find({}, {sort: {createdAt: -1}});
+
+			}
+		},
+		hideCompleted: function(){
+
+			return Session.get("hideCompleted");
+		},
+		incompleteCount: function(){ // Add to Template.body.helpers
+
+			return Tasks.find({checked: {$ne: true}}).count();
+
+		},
+		completedCount: function(){
+
+			return ( Tasks.find({}).count() - Tasks.find({checked: {$ne: true}}).count() );
+
+		}
+
+	});
 
 
-  // Events
-  Template.listItem.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
-    },
-    "click .delete": function () {
-      Tasks.remove(this._id);
-    }
-  });
+	Template.body.helpers({
+		tasks: function () {
+			if (Session.get("hideCompleted")) {
+				// If hide completed is checked, filter tasks
+				return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+			} else {
+				// Otherwise, return all of the tasks
+				return Tasks.find({}, {sort: {createdAt: -1}});
+			}
+		},
+		hideCompleted: function() {
+			return Session.get("hideCompleted");
+		}
+	});
 
-  // Submit Query to Database
-  Template.newTaskForm.events({
-    "submit #new-task": function(event){
 
-      // This function is called when the new task form is submitted
-      event.preventDefault(); // Applied this made better UX
+	// Events
+	Template.listItem.events({
+		"click .toggle-checked": function() {
+			// Set the checked property to the opposite of its current value
+			Tasks.update(this._id, {$set: {checked: ! this.checked}});
+		},
+		"click .delete": function() {
+			Tasks.remove(this._id);
+		}
+	});
 
-      // Select IDs
-      var taskField = document.getElementById("task");
+	// Submit Query to Database
+	Template.newTaskForm.events({
+		"submit #new-task": function(event){
 
-      // Values
-      var taskField_text = taskField.value;
+			// This function is called when the new task form is submitted
+			event.preventDefault(); // Applied this made better UX
 
-      // Store to Database
-      Tasks.insert({
-        text: taskField_text,
-        createdAt: new Date() // current time
-      });
+			// Select IDs
+			var taskField = document.getElementById("task");
 
-      // Clear form
-      taskField.value = "";
+			// Values
+			var taskField_text = taskField.value;
 
-      // Prevent default form submit
-      return false;
-    }
- 
-  }); 
+			// Store to Database
+			Tasks.insert({
+				text: taskField_text,
+				createdAt: new Date() // current time
+			});
 
-  $(document).on('ready',function(){
-    console.log( 'jQuery Initialized' );
-  });
+			// Clear form
+			taskField.value = "";
 
-}
+			// Prevent default form submit
+			return false;
+		}
+	}); 
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	$(document).on('ready',function(){
+		console.log( 'jQuery Initialized' );
+	});
+
+	}
+
+
+if(Meteor.isServer) {
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
 }
 
 
